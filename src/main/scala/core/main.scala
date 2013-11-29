0418014c281050747082cd0d2cb8c27a4bb3275a
Jan Machacek <jan.machacek@gmail.com>
2013-11-29 12:19:19 +0000

    Use the actors in Main

--- src/main/scala/core/main.scala
+++ src/main/scala/core/main.scala
@@ -8,9 +8,15 @@ import akka.io.IO
 object Main extends App {
   import Commands._
 
+  val system = ActorSystem()
+  val sentiment = system.actorOf(Props(new SentimentAnalysisActor))
+  val stream = system.actorOf(Props(new TweetStreamerActor(TweetStreamerActor.twitterUri, sentiment)))
+
   @tailrec
   private def commandLoop(): Unit = {
     Console.readLine() match {
+      case QuitCommand         => return
+      case TrackCommand(query) => stream ! query
       case _                   => println("WTF??!!")
     }
 
