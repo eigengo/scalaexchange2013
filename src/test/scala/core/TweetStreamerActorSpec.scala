b369fabec1ec2511e6016f43616554a7c2dc0e9d
Jan Machacek <jan.machacek@gmail.com>
2013-11-29 12:36:47 +0000

    Unmarshalling and tests

--- src/test/scala/core/TweetStreamerActorSpec.scala
+++ src/test/scala/core/TweetStreamerActorSpec.scala
@@ -11,4 +11,23 @@ import akka.io.IO
 class TweetStreamerActorSpec extends TestKit(ActorSystem()) with SpecificationLike with ImplicitSender {
   sequential
 
+  val port = 12345
+  val tweetStream = TestActorRef(new TweetStreamerActor(Uri(s"http://localhost:$port/"), testActor))
+
+  "Getting all 'typesafe' tweets" >> {
+
+    "should receive the tweets" in {
+      val twitterApi = TwitterApi(port)
+      tweetStream ! "typesafe"
+      Thread.sleep(1000)
+      val tweet = expectMsgType[Tweet]
+      tweet.text mustEqual "Aggressive Ponytail #freebandnames"
+      tweet.user.lang mustEqual "en"
+      tweet.user.id mustEqual "137238150"
+      tweet.place mustEqual None
+      twitterApi.stop()
+      success
+    }
+  }
+
 }
